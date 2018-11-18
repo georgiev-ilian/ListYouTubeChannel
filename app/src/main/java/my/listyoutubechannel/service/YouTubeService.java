8afec80bf1a6e55d4b31a82fc6486199f153b802
Ilian Georgiev <georgiev.ilian@gmail.com>
2018-11-18 16:48:35 +0200

    Add Rx adapter to YouTubeService class.

--- app/src/main/java/my/listyoutubechannel/service/YouTubeService.java
+++ app/src/main/java/my/listyoutubechannel/service/YouTubeService.java
@@ -2,6 +2,7 @@ package my.listyoutubechannel.service;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -12,6 +13,10 @@ import retrofit2.http.Query;
 public class YouTubeService {
 
     // https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_A--fhX5gea0i4UtpD99Gg&maxResults=20&key=AIzaSyAEV6gn8EKbAL9uQfX8XfrYG3v3vsIqEY8
+
+    // https://www.googleapis.com/youtube/v3/videos?part=snippet&id=kX_hn3Xf90g&key=AIzaSyAEV6gn8EKbAL9uQfX8XfrYG3v3vsIqEY8
+
+    // https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=kX_hn3Xf90g&key=AIzaSyAEV6gn8EKbAL9uQfX8XfrYG3v3vsIqEY8
 
     private static final String API_URL = "https://www.googleapis.com/youtube/v3/";
 
@@ -24,8 +29,9 @@ public class YouTubeService {
     }
 
     public static YouTube create() {
+        RxJava2CallAdapterFactory rxAdapter = RxJava2CallAdapterFactory.create();
         Retrofit retrofit = new Retrofit.Builder().baseUrl(API_URL).addConverterFactory(
-                GsonConverterFactory.create()).build();
+                GsonConverterFactory.create()).addCallAdapterFactory(rxAdapter).build();
 
         return retrofit.create(YouTube.class);
     }
