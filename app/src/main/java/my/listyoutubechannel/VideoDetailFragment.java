a9723d3b24d4645773da8d649080d053e5378e94
Ilian Georgiev <georgiev.ilian@gmail.com>
2018-11-18 23:58:51 +0200

    Add navigation safe args plugin.

--- app/src/main/java/my/listyoutubechannel/VideoDetailFragment.java
+++ app/src/main/java/my/listyoutubechannel/VideoDetailFragment.java
@@ -1,5 +1,6 @@
 package my.listyoutubechannel;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 /**
@@ -7,4 +8,15 @@ import android.support.v4.app.Fragment;
  */
 public class VideoDetailFragment extends Fragment {
 
+    private static final String ARG_ITEM_ITEM = "item_id";
+
+    public static VideoDetailFragment newInstance(String videoId) {
+        Bundle args = new Bundle();
+        args.putString(ARG_ITEM_ITEM, videoId);
+
+        VideoDetailFragment fragment = new VideoDetailFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 }
