4802e713e271e4fa330649ebab68577c23713261
Ilian Georgiev <georgiev.ilian@gmail.com>
2018-11-18 16:58:23 +0200

    Add video id.

--- app/src/main/java/my/listyoutubechannel/VideoListItem.java
+++ app/src/main/java/my/listyoutubechannel/VideoListItem.java
@@ -10,13 +10,24 @@ import com.bumptech.glide.Glide;
  */
 public class VideoListItem {
 
+    private String id;
+
     private String title;
 
     private String url;
 
-    VideoListItem(String title, String url) {
+    VideoListItem(String id, String title, String url) {
+        this.id = id;
         this.title = title;
         this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
