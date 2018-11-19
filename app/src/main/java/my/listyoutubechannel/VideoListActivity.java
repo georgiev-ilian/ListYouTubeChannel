dc2c53344a058da4cad380ab3d296ced9ba7895d
Ilian Georgiev <georgiev.ilian@gmail.com>
2018-11-19 09:02:49 +0200

    Fix main activity toolbar.

--- app/src/main/java/my/listyoutubechannel/VideoListActivity.java
+++ app/src/main/java/my/listyoutubechannel/VideoListActivity.java
@@ -1,13 +1,19 @@
 package my.listyoutubechannel;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import my.listyoutubechannel.databinding.ActivityVideoListBinding;
 
 public class VideoListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_list);
+
+        ActivityVideoListBinding binding = DataBindingUtil.setContentView(this,
+                                                                          R.layout.activity_video_list);
+        setSupportActionBar(binding.toolbar);
     }
 }
